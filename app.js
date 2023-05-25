eaebe696884ede44138c10599bce0e0af5c1e2eb
udithanayanajith <uditha.nayanagith@gmail.com>
2023-05-25 18:18:47 +0530

    Only 2 apis

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
+const port = 3000;
+
+// Local Connection
+// const dbConfig = mysql.createConnection({
+//   host: "localhost",
+//   database: "medi",
+//   user: "root",
+//   password: "",
+// });
+
+// Hosted Connection
+const dbConfig = mysql.createConnection({
+  host: "sql9.freesqldatabase.com",
+  database: "sql9621204",
+  user: "sql9621204",
+  password: "eMxsvJk2lv",
+});
+dbConfig.connect((err) => {
+  if (err) {
+    console.log(err);
+    throw err;
+  } else {
+    console.log("Database Connected");
+  }
+});
+
+//ViewAllDrugs
+app.get("/allDrugs", (req, res) => {
+  const searchTerm = req.query.item;
+  let sql = `SELECT * FROM drugs  `;
+
+  dbConfig.query(sql, (err, rows) => {
+    if (err) {
+      console.log(err);
+      throw err;
+    } else {
+      res.json(rows);
+    }
+  });
+});
+//SerarchAllDrugs
+app.get("/searchDrugs", (req, res) => {
+  const searchTerm = req.query.term;
+  let sql = `SELECT * FROM drugs WHERE d_name LIKE '${searchTerm}%' OR d_brand LIKE '${searchTerm}%' `;
+
+  dbConfig.query(sql, (err, rows) => {
+    if (err) {
+      console.log(err);
+      throw err;
+    } else {
+      res.json(rows);
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log("App is running on port :", port);
+});
