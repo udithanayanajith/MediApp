b5c52e0033245a31855aa729166a667f2b577fe5
udithanayanajith <uditha.nayanagith@gmail.com>
2023-05-26 02:25:54 +0530

    addDrugs API created

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const mysql = require("mysql");
 const app = express();
 const port = 3000;
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(express.json());
+
 // Local Connection
 // const dbConfig = mysql.createConnection({
 //   host: "localhost",
@@ -40,17 +44,40 @@ app.get("/allDrugs", (req, res) => {
     }
   });
 });
-//SerarchAllDrugs
+//SerarchAllDrugs InsideBody
 app.get("/searchDrugs", (req, res) => {
-  const searchTerm = req.query.item;
+  const searchTerm = req.body.item;
   let sql = `SELECT * FROM drugs WHERE d_name LIKE '${searchTerm}%' OR d_brand LIKE '${searchTerm}%' `;
 
   dbConfig.query(sql, (err, rows) => {
     if (err) {
       console.log(err);
+      // res.json("There is no such kind of Drug name or brand");
+      res.json("Error");
       throw err;
     } else {
-      res.json(rows);
+      if (rows && rows.length > 0) {
+        res.json(rows);
+      } else {
+        res.json("There is no such kind of Drug name or brand");
+      }
+    }
+  });
+});
+
+//Insert Drugs
+app.post("/addDrugs", (req, res) => {
+  const { d_name, d_brand } = req.body;
+  const sql = `INSERT INTO drugs (d_name, d_brand) VALUES ('${d_name}','${d_brand}')`;
+
+  dbConfig.query(sql, (error, results) => {
+    if (error) {
+      console.error(error);
+      res
+        .status(500)
+        .json({ error: "Failed to insert data into the database." });
+    } else {
+      res.status(200).json({ message: "Data inserted successfully." });
     }
   });
 });
