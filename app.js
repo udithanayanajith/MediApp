3e14bc687a79c4cb53b05f401b90507c6a4e9732
udithanayanajith <uditha.nayanagith@gmail.com>
2023-06-04 18:03:23 +0530

    Adding cors

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require("express");
 const mysql = require("mysql");
 const app = express();
 const port = 3000;
-
+const cors = require("cors");
 const bodyParser = require("body-parser");
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
