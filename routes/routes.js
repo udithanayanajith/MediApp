d7cafb51eded54b13a4303c451055060fe9fcdcc
Uditha Nayanajith <70725375+udithanayanajith@users.noreply.github.com>
2023-06-06 12:56:08 +0530

    Update routes.js login req get to post

--- routes/routes.js
+++ routes/routes.js
@@ -30,7 +30,7 @@ router.post("/signup", async (req, res) => {
 
 //Login
 
-router.get("/login", async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
     const { username, password } = req.body;
 
